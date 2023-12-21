08eb11c20678382740e265040b7899f2d896ea6e
“Arunthamizh” <arunthamizh.azhagiri@twilightitsolutions.com>
2023-12-21 11:38:08 +0530

    06th-video using NavLinks

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import classes from './MainHeader.module.css'
 const MainHeader = () => {
   return (
-    <header>
+    <header className={classes.header}>
         <nav>
             <ul>
                 <li>
-                    <Link to='/welcome'>Welcome</Link>
+                    {/* <a href='#'>Home</a> // !this will not prevent the browser default action so page refresh */} 
+                    {/* <Link to='/welcome'>Welcome</Link> // ! Link will prevent the browser default action */}
+                    {/* // ! NavLink will show the active Link by adding the class on it */}
+                    <NavLink to='/welcome' className={(navData) => navData.isActive ? classes.active : ''} >Welcome</NavLink> 
                 </li>
                 <li>
-                    <Link to='/products'>Products</Link>
+                    <NavLink to='/products' className={(navData) => navData.isActive ? classes.active : ''}>Products</NavLink>
                 </li>
             </ul>
         </nav>
